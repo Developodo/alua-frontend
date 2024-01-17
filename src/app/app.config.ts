9f65c9b7c2c07dc6d34ec23bad641d13575ab43c
Carlos Serrano <carlosserrano@MacBook-Pro-de-Carlos.local>
2024-01-18 00:05:23 +0100

    pwa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -6,11 +6,20 @@ import { provideHttpClient } from '@angular/common/http';
 import { StravaService } from './services/strava.service';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { MAT_DATE_LOCALE } from '@angular/material/core';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes),
     provideHttpClient(), importProvidersFrom(StravaService), provideAnimations(),
-    { provide: MAT_DATE_LOCALE, useValue: 'es-ES' }
-    ,
-    provideRouter(routes, withComponentInputBinding())]
+    { provide: MAT_DATE_LOCALE, useValue: 'es-ES' },
+    provideRouter(routes, withComponentInputBinding()), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    }), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    }), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })]
 };
