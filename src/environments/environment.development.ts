4a0493bcd8a7379029ba0f55c828d105255dfd38
Carlos Serrano <carlosserrano@MacBook-Pro-de-Carlos.local>
2024-01-18 18:33:20 +0100

    BUG

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     urlApi: 'https://alua-backend.vercel.app/',
     //frontURL: 'https://localhost/'
-    frontURL: 'https://alua-frontend.vercel.app/'
+    frontURL: 'https://alua.vercel.app/'
 };
